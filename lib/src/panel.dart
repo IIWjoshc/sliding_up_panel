ca1147c1e05f9a1219f22108624ea2113852da84
Josh Cundiff <joshc@iiw.org>
2020-04-02 13:22:42 -0400

    added blur to backdrop overlay

--- lib/src/panel.dart
+++ lib/src/panel.dart
@@ -266,17 +266,19 @@ class _SlidingUpPanelState extends State<SlidingUpPanel> with SingleTickerProvid
         //the backdrop to overlay on the body
         !widget.backdropEnabled ? Container() : GestureDetector(
           onTap: widget.backdropTapClosesPanel ? _close : null,
-          child: FadeTransition(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: (_ac.value * 10), sigmaY: (_ac.value * 10)),
+            child: FadeTransition(
             opacity: Tween(begin: 0.0, end: widget.backdropOpacity).animate(_ac),
             child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-
-              //set color to null so that touch events pass through
-              //to the body when the panel is closed, otherwise,
-              //if a color exists, then touch events won't go through
-              color: _ac.value == 0.0 ? null : widget.backdropColor,
-            ),
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                //set color to null so that touch events pass through
+                //to the body when the panel is closed, otherwise,
+                //if a color exists, then touch events won't go through
+                color: _ac.value == 0.0 ? null : widget.backdropColor,
+                ),
+              ),
           ),
         ),
 
